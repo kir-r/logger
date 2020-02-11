8cf5e3d021adf38d190cd85ba30b312d4bdfdb86
IgorKey <Igor_Kuzminykh@epam.com>
2020-02-11 12:46:51 +0300

    updated project native structures by com.epam.drill.cross-compilation plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,42 @@
 plugins {
-    id("kotlin-multiplatform")
-    id("kotlinx-serialization")
+    kotlin("multiplatform")
+    id("com.epam.drill.cross-compilation") version "0.13.0"
     `maven-publish`
 }
 
 repositories {
     mavenCentral()
-    jcenter()
+    maven(url = "https://dl.bintray.com/kotlin/kotlinx/")
 }
 
 kotlin {
-    targets {
-        if (isDevMode)
-            currentTarget {
-                compilations["main"].apply {
-                    defaultSourceSet {
-                        kotlin.srcDir("./src/nativeCommonMain/kotlin")
-                        applyDependencies()
-                    }
-                }
-            }
-        else {
-            setOf(linuxX64(), macosX64(), mingwX64())
-        }
-        jvm()
 
+    crossCompilation {
+        common {
+            defaultSourceSet.dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:$serializationRuntimeVersion")
+                implementation("io.ktor:ktor-utils-native:$ktorUtilVersion")
+            }
+        }
     }
+    macosX64()
+    mingwX64()
+    linuxX64()
 
-    sourceSets {
-        jvm {
-            compilations["main"].defaultSourceSet {
-                dependencies {
-                    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$serializationRuntimeVersion")
-                    implementation("io.github.microutils:kotlin-logging:$loggingVersion")
-                }
-                compilations["test"].defaultSourceSet {
-                    dependencies {
-                        implementation(kotlin("test-junit"))
-                    }
-                }
+    jvm {
+        compilations["main"].defaultSourceSet {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$serializationRuntimeVersion")
+                implementation("io.github.microutils:kotlin-logging:$loggingVersion")
             }
         }
+        compilations["test"].defaultSourceSet {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+    }
+    sourceSets {
         commonMain {
             dependencies {
                 implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
@@ -53,54 +49,6 @@ kotlin {
             dependencies {
                 implementation("org.jetbrains.kotlin:kotlin-test-common")
                 implementation("org.jetbrains.kotlin:kotlin-test-annotations-common")
-            }
-        }
-
-        if (!isDevMode) {
-            val commonNativeMain = maybeCreate("nativeCommonMain")
-            targets.filterIsInstance<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget>().forEach {
-                it.compilations.forEach { knCompilation ->
-                    if (knCompilation.name == "main")
-                        knCompilation.defaultSourceSet {
-                            dependsOn(commonNativeMain)
-                            applyDependencies()
-                        }
-                }
-            }
-        }
-    }
-}
-
-tasks.withType<AbstractTestTask> {
-    testLogging.showStandardStreams = true
-}
-tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinCompile<org.jetbrains.kotlin.gradle.dsl.KotlinCommonOptions>> {
-    kotlinOptions.freeCompilerArgs += "-Xuse-experimental=kotlinx.serialization.ImplicitReflectionSerializer"
-}
-tasks.build {
-    dependsOn("publishToMavenLocal")
-}
-
-fun org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet.applyDependencies() {
-    dependencies {
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:$serializationRuntimeVersion")
-        implementation("io.ktor:ktor-utils-native:$ktorUtilVersion")
-    }
-}
-
-publishing {
-    repositories {
-        maven {
-            url = uri("https://oss.jfrog.org/oss-release-local")
-            credentials {
-                username =
-                        if (project.hasProperty("bintrayUser"))
-                            project.property("bintrayUser").toString()
-                        else System.getenv("BINTRAY_USER")
-                password =
-                        if (project.hasProperty("bintrayApiKey"))
-                            project.property("bintrayApiKey").toString()
-                        else System.getenv("BINTRAY_API_KEY")
             }
         }
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,7 @@
 rootProject.name = "logger"
-
+pluginManagement {
+    repositories {
+        maven(url = "http://oss.jfrog.org/oss-release-local")
+        gradlePluginPortal()
+    }
+}
